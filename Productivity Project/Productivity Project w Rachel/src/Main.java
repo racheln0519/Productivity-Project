3e27bff6e7f724becdc2219c9cd69cb9f0c2f501
Rachel Ng <racheln0519@gmail.com>
2023-02-05 11:18:37 -0800

    O

--- Productivity Project/Productivity Project w Rachel/src/Main.java
+++ Productivity Project/Productivity Project w Rachel/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int i = 0;
+        System.out.println("Hello");
+        System.out.println("Hello World!");
     }
 
-    int i = 0;
-    System.out.println("Hello");
+
     
 }
