007f8ef9bf83ab0fdb44dc113364aeea29219067
Rachel Ng <racheln0519@gmail.com>
2023-02-08 19:42:20 -0800

    BUTTON

---
+++ Productivity Project/src/GUI.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+
+    private JButton button = new JButton("BUTTON");
+
+    public GUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Productivity Project");
+        getContentPane().setBackground(new java.awt.Color(147, 201, 147));
+
+        // Sets the location and size of the frame and makes it visible
+        setSize(400, 500);
+        setLocationRelativeTo(null);
+
+        setLayout(new GridLayout(3, 1));
+
+        JPanel panel = new JPanel();
+        add(panel);
+        panel.add(button);
+    }
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("New Frame");
+    }
+}
