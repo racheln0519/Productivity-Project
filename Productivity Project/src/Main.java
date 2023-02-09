4324055c726972684be445a8eebbbdb40b05395f
Christmas19307 <116537273+Christmas19307@users.noreply.github.com>
2023-02-08 22:35:07 -0500

    Added JFrame

--- Productivity Project/src/Main.java
+++ Productivity Project/src/Main.java
@@ -1,11 +1,6 @@
+
 public class Main {
     public static void main(String[] args) {
-
-        int i = 0;
-        System.out.println("Hello");
-        System.out.println("Hello World!");
+        JFrame frame = new JFrame("New Frame");
     }
-
-
-    
 }
---
+++ Productivity Project/src/Test.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Test extends JFrame{
+
+
+    public static void main(String [] args) {
+
+    }
+
+    public GUI(){
+
+        private JButton button2;
+        GUI gui= new GUI();
+    }
+}
